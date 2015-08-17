c72e841b5cdaaed0c1331e39af5b6613ff643dbf
andrewcparnell <andrew.parnell@gmail.com>
2015-08-17 10:06:40 +0100

    Removed shiny directory from master branch

--- inst/simmr_shiny/simmr_shiny.R
+++
@@ -1,9 +0,0 @@
-#' @export
-simmr_shiny <- function() {
-  appDir <- system.file("simmr_shiny", "simmr_shiny", package = "simmr")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
-  }
-  
-  shiny::runApp(appDir, display.mode = "normal")
-}
