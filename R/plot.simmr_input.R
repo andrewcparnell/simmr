a2634ab43e898c9ccb3dcf7d1317de3368e8728c
Andrew Parnell <andrew.parnell@gmail.com>
2017-07-28 12:00:57 +0100

    Created new plot.simmr_input for 1D plots with example in vignette

--- R/plot.simmr_input.R
+++ R/plot.simmr_input.R
@@ -1,5 +1,12 @@
 plot.simmr_input <-
-function(x,tracers=c(1,2),title='Tracers plot',xlab=colnames(x$mixtures)[tracers[1]],ylab=colnames(x$mixtures)[tracers[2]], sigmas=1, group=1, mix_name = 'Mixtures', colour=TRUE,...) {
+function(x,tracers = c(1,2),
+         title = 'Tracers plot',
+         xlab = colnames(x$mixtures)[tracers[1]],
+         ylab = colnames(x$mixtures)[tracers[2]], 
+         sigmas=1, 
+         group=1, 
+         mix_name = 'Mixtures', 
+         colour=TRUE, ...) {
 
 # Get mixtures to match current group(s)
 curr_rows = which(x$group%in%group)  
@@ -10,7 +17,7 @@ curr_n_groups = length(group)
 #if((length(group)+x$n_sources)>6) stop("Too many groups specified. Total number of groups plus number of sources cannot exceed 6")
 
 # Throw error if plotting only one isotope
-if(ncol(curr_mix)==1) stop("This function only works for two or more tracers")
+#if(ncol(curr_mix)==1) stop("This function only works for two or more tracers")
 
 # First get the mean corrected sources and the sd corrected sources
 source_means_c = x$source_means + x$correction_means
@@ -18,41 +25,84 @@ source_sds_c = sqrt(x$source_sds^2 + x$correction_sds^2)
 
 # Set up data frame for ggplot - have to do it this stupid way because of cran
 x2=c(source_means_c[,tracers[1]],curr_mix[,tracers[1]])
-y=c(source_means_c[,tracers[2]],curr_mix[,tracers[2]])
 x_lower=c(source_means_c[,tracers[1]]-sigmas*source_sds_c[,tracers[1]],curr_mix[,tracers[1]])
 x_upper=c(source_means_c[,tracers[1]]+sigmas*source_sds_c[,tracers[1]],curr_mix[,tracers[1]])
-y_lower=c(source_means_c[,tracers[2]]-sigmas*source_sds_c[,tracers[2]],curr_mix[,tracers[2]])
-y_upper=c(source_means_c[,tracers[2]]+sigmas*source_sds_c[,tracers[2]],curr_mix[,tracers[2]])
+
+if(ncol(curr_mix)>1) {
+  y=c(source_means_c[,tracers[2]],curr_mix[,tracers[2]])
+  y_lower=c(source_means_c[,tracers[2]]-sigmas*source_sds_c[,tracers[2]],curr_mix[,tracers[2]])
+  y_upper=c(source_means_c[,tracers[2]]+sigmas*source_sds_c[,tracers[2]],curr_mix[,tracers[2]])
+}
+
 if(x$n_groups==1) {
   Source=factor(c(x$source_names,rep(mix_name,nrow(curr_mix))),levels=c(mix_name,x$source_names))
 } else {
   Source=factor(c(x$source_names,paste(mix_name,'grp',x$group[curr_rows])),levels=c(paste(mix_name,'grp',unique(x$group[curr_rows])),x$source_names))
 }
 size=c(rep(0.5,x$n_sources),rep(0.5,nrow(curr_mix)))
-df=data.frame(x=x2,y=y,x_lower,y_lower,x_upper,y_upper,Source,size)
 
-if(colour) {
-  g=ggplot(data=df, aes(x = x,y = y,colour=Source)) + 
-    scale_color_viridis(discrete=TRUE) + 
-    theme_bw() +
-    labs(x=xlab,y=ylab,title=title) +
-    geom_errorbarh(aes(xmax=x_upper,xmin=x_lower,height=0)) +
-    #geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,height=0.2,shape=Source)) +
-    geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,shape=Source)) +
-    scale_shape_manual(values=1:nlevels(df$Source)) +
-    theme(legend.title=element_blank(),legend.key = element_blank()) +
-    guides(color=guide_legend(override.aes=list(linetype=c(rep(0,curr_n_groups),rep(1,x$n_sources))))) 
+if(ncol(mix) == 1) {
+  df=data.frame(x=x2,x_lower,x_upper,Source,size, y = Source)
 } else {
-  g=ggplot(data=df, aes(x = x,y = y,colour=Source)) + 
-    theme_bw() +
-    labs(x=xlab,y=ylab,title=title) +
-    geom_errorbarh(aes(xmax=x_upper,xmin=x_lower,height=0)) +
-    #geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,height=0.2,shape=Source)) +
-    geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,shape=Source)) +
-    scale_shape_manual(values=1:nlevels(df$Source)) +
-    theme(legend.title=element_blank(),legend.key = element_blank()) +
-    guides(color=guide_legend(override.aes=list(linetype=c(rep(0,curr_n_groups),rep(1,x$n_sources))))) + 
-    scale_colour_grey()
+  df=data.frame(x=x2,y=y,x_lower,y_lower,x_upper,y_upper,Source,size)
+}
+
+# Plot for bivariate mixtures
+if(ncol(mix) > 1) {
+  if(colour) {
+    g=ggplot(data=df, aes(x = x,y = y,colour=Source)) + 
+      scale_color_viridis(discrete=TRUE) + 
+      theme_bw() +
+      labs(x=xlab,y=ylab,title=title) +
+      geom_errorbarh(aes(xmax=x_upper,xmin=x_lower,height=0)) +
+      #geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,height=0.2,shape=Source)) +
+      geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,shape=Source)) +
+      scale_shape_manual(values=1:nlevels(df$Source)) +
+      theme(legend.title=element_blank(),legend.key = element_blank()) +
+      guides(color=guide_legend(override.aes=list(linetype=c(rep(0,curr_n_groups),rep(1,x$n_sources))))) 
+  } else {
+    g=ggplot(data=df, aes(x = x,y = y,colour=Source)) + 
+      theme_bw() +
+      labs(x=xlab,y=ylab,title=title) +
+      geom_errorbarh(aes(xmax=x_upper,xmin=x_lower,height=0)) +
+      #geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,height=0.2,shape=Source)) +
+      geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,shape=Source)) +
+      scale_shape_manual(values=1:nlevels(df$Source)) +
+      theme(legend.title=element_blank(),legend.key = element_blank()) +
+      guides(color=guide_legend(override.aes=list(linetype=c(rep(0,curr_n_groups),rep(1,x$n_sources))))) + 
+      scale_colour_grey()
+  }
+}
+
+# Plot for univariate mixtures
+if(ncol(mix) == 1) {
+  if(colour) {
+    g = ggplot(data=df, aes(x = x, y = y, colour = Source)) + 
+      scale_color_viridis(discrete=TRUE) + 
+      theme_bw() +
+      theme(axis.title.y=element_blank()) +
+      labs(x = xlab, title = title) +
+      geom_errorbarh(aes(xmax=x_upper,xmin=x_lower,height=0)) +
+      #geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,height=0.2,shape=Source)) +
+      geom_point(aes(shape = Source)) +
+      scale_shape_manual(values=1:nlevels(df$Source)) +
+      theme(legend.position = 'None') +
+      guides(color=guide_legend(override.aes=list(linetype=c(rep(0,curr_n_groups),rep(1,x$n_sources))))) 
+  } else {
+    g = ggplot(data=df, aes(x = x, y = y, colour = Source)) + 
+      scale_color_grey() + 
+      theme_bw() +
+      theme(axis.title.y=element_blank(),
+            axis.text.y=element_blank(),
+            axis.ticks.y=element_blank()) +
+      labs(x = xlab, title = title) +
+      geom_errorbarh(aes(xmax=x_upper,xmin=x_lower,height=0)) +
+      #geom_pointrange(aes(x=x,y=y,ymax=y_upper,ymin=y_lower,height=0.2,shape=Source)) +
+      geom_point(aes(shape = Source)) +
+      scale_shape_manual(values=1:nlevels(df$Source)) +
+      theme(legend.title=element_blank(),legend.key = element_blank()) +
+      guides(color=guide_legend(override.aes=list(linetype=c(rep(0,curr_n_groups),rep(1,x$n_sources))))) 
+  }
 }
 
 print(g)
