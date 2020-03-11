77e14b355a13463c6b8e43dd847f44a381b16178
Andrew Parnell <andrew.parnell@mu.ie>
2020-03-11 11:33:03 +0000

    Updated tests

--- tests/testthat/test_simmr_input_plots.R
+++
@@ -1,2 +0,0 @@
-context('Plot input data') 
-
--- tests/testthat_r_z.R
+++
@@ -1,8 +0,0 @@
-library(testthat)
-library(simmr)
-
-# broke up tests to allow tests to run longer than 10 min in travis
-# see https://github.com/travis-ci/travis-ci/issues/3849#issuecomment-345686242
-# note that if tests in test that script do not start with "test.", they will not
-# run
-test_check("simmr", filter = "^test.[r-z]")
