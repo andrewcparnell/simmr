867674a1d273d996770667a7e703fc96de1e6053
Emma Govan <67744546+emmagovan@users.noreply.github.com>
2023-03-04 13:30:53 +0000

    added ggpairs_fn to replace matrix function in plot_output

--- R/plot.simmr_output.R
+++ R/plot.simmr_output.R
@@ -160,55 +160,25 @@ if(inherits(x, "simmr_output") == TRUE){
       # }
 
       if ("matrix" %in% type) {
-        # These taken from the help(pairs) file
-        # panel.hist <- function(x, ...) {
-        #   usr <- graphics::par("usr")
-        #   on.exit(graphics::par(usr))
-        #   graphics::par(usr = c(usr[1:2], 0, 1.5))
-        #   h <- graphics::hist(x, plot = FALSE)
-        #   breaks <- h$breaks
-        #   nB <- length(breaks)
-        #   y <- h$counts
-        #   y <- y / max(y)
-        #   graphics::rect(breaks[-nB], 0, breaks[-1], y, col = "lightblue", ...)
-        # }
-        panel.hist <- function(x, ...)
-        {
-          usr <- graphics::par("usr")
-          graphics::par(usr = c(usr[1:2], 0, 1.5) )
-          h <- hist(x, plot = FALSE)
-          breaks <- h$breaks; nB <- length(breaks)
-          y <- h$counts; y <- y/max(y)
-          rect(breaks[-nB], 0, breaks[-1], y, col = "lightblue", ...)
+         modified_bar = function(data, mapping, ...) {
+          GGally::ggally_barDiag(data, mapping, ..., binwidth = 0.025)  + scale_x_continuous(limits = c(0, 1)) +theme_bw()
         }
-        # panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...) {
-        #   usr <- graphics::par("usr")
-        #   on.exit(graphics::par(usr))
-        #   graphics::par(usr = c(0, 1, 0, 1))
-        #   r <- stats::cor(x, y)
-        #   txt <- format(c(r, 0.123456789), digits = digits)[1]
-        #   txt <- paste0(prefix, txt)
-        #   if (missing(cex.cor)) cex.cor <- 0.8 / graphics::strwidth(txt)
-        #   graphics::text(0.5, 0.5, txt, cex = cex.cor * abs(r))
-        # }
-        panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
-        {
-          par(usr = c(0, 1, 0, 1))
-          r <- abs(cor(x, y))
-          txt <- format(c(r, 0.123456789), digits = digits)[1]
-          txt <- paste0(prefix, txt)
-          if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
-          text(0.5, 0.5, txt, cex = cex.cor * r)
+        modified_density = function(data, mapping, ...){
+          ggplot(data = data, mapping = mapping, ...)+ stat_density_2d(geom = "polygon", contour = TRUE,
+                                                                       aes(fill = after_stat(level)), colour = "black",
+                                                                       bins = 5) +
+            scale_fill_distiller(palette = "Blues", direction = 1) +
+            theme_bw()+ scale_x_continuous(limits = c(0, 1)) +scale_y_continuous(limits = c(0,1))
         }
-        panel.contour <- function(x, y, ...) {
-          usr <- graphics::par("usr")
-          #on.exit(graphics::par(usr))
-          graphics::par(usr = c(usr[1:2], 0, 1.5))
-          kd <- MASS::kde2d(x, y)
-          kdmax <- max(kd$z)
-          graphics::contour(kd, add = TRUE, drawlabels = FALSE, levels = c(kdmax * 0.1, kdmax * 0.25, kdmax * 0.5, kdmax * 0.75, kdmax * 0.9))
-        }
-        graphics::pairs(out_all, xlim = c(0, 1), ylim = c(0, 1), main = title[i], diag.panel = panel.hist, lower.panel = panel.cor, upper.panel = panel.contour)
+
+      g<-GGally::ggpairs(data.frame(out_all),
+                   upper = list(continuous = GGally::wrap(modified_density)),
+                   diag = list(continuous = GGally::wrap(modified_bar)), 
+                   lower = list(continuous = GGally::wrap("cor", stars = FALSE)))
+        
+        
+        
+        print(g)
       }
     }
     if (exists("g")) invisible(g)
